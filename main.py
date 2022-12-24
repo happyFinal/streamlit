f766976ff596943d022439713b8f3d0360566155
“dk-davidekim <dk.davidekim@gmail.com>
2022-12-25 04:31:41 +0900

    main.py update

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ with row0_2:
     st.subheader(
         "해파리팀"
     )
-    st.write("이지혜, 최지영, 권소희, 문종현, 구자현, 김의준")
+    st.write("이지혜, 최지영, 권소희")
+    st.write("문종현, 구자현, 김의준")
 
 st.write('---')
 
@@ -47,13 +48,13 @@ row1_spacer1, row1_1, row1_spacer2 = st.columns((0.01, 3, 0.01))
 
 with row1_1:
     st.markdown(
-        "**'MZ세대', 음악의 세대.**"
+        "**'MZ세대'에게**"
     )
     st.markdown(
-        "MZ세대에게 음악은 세대를 드러내는 지표이자 자신의 감정 및 공동체를 드러내는 수단."
+        "음악은 세대를 드러내는 지표이자 자신의 감정 및 공동체를 드러내는 수단이다."
     )
     st.markdown(
-        "우리는 음악 가사라 일컬어지는 우리들의 문화를 드러내고 단순히 목적을 위한 사용이 아니라 속한 세대의 특징을 드러낼 수 있는 ‘즐거움’이라는 키워드를 상정하고 이에 걸맞는 프로젝트를 하고자 했다. 그에 따라 sns를 통해 소통하는 MZ세대가 즐겁게 향유할 수 있는 단순하지만 공유를 통해 연결성을 드러낼 수 있는 방식으로 노래 가사 데이터 기반 N행시 생성기를 기획하게 되었다."
+        "우리는 음악 가사라 일컬어지는 우리들의 문화를 드러내고 단순히 목적을 위한 사용이 아니라 속한 세대의 특징을 드러낼 수 있는 ‘즐거움’이라는 키워드를 상정하고 이에 걸맞는 프로젝트를 하고자 했다. 그에 따라 sns를 통해 소통하는 MZ세대가 즐겁게 향유할 수 있는 단순하지만 공유를 통해 연결성을 드러낼 수 있는 방식으로 노래 가사 데이터 기반 N행시 생성기를 제작하게 되었다."
     )
 
 st.write('---')
@@ -78,6 +79,9 @@ with row2_1:
     )
 
 # Word Input
+if "generate" not in st.session_state:
+    st.session_state.generate = False
+
 with row2_2:
     word_input = st.text_input(
             "n행시에 사용할 단어를 적어주세요. 👇",
@@ -87,6 +91,11 @@ with row2_2:
     if word_input:
         st.write("n행시 단어 :  ", word_input)
 
+    if st.button('n행시 제작하기'):
+        if word_input.empty():
+            st.write("사용할 단어를 적고 Enter를 눌러주세요.")
+        else:
+            generate = True
 
 
 
