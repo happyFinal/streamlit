ebbab841288e0407749fa16a7825f07c9189e4d3
“dk-davidekim <dk.davidekim@gmail.com>
2022-12-25 03:11:29 +0900

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import requests
+from streamlit_lottie import st_lottie
+
+st.set_page_config(
+    page_title="노래 가사 n행시",
+    page_icon="💌",
+)
+
+@st.cache
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url = "https://assets7.lottiefiles.com/private_files/lf30_fjln45y5.json"
