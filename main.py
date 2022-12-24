326c530c032e4bb089c8ec3a2939f4b9a847b5e2
“dk-davidekim <dk.davidekim@gmail.com>
2022-12-25 03:50:31 +0900

    main.py update

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ lottie_url = "https://assets7.lottiefiles.com/private_files/lf30_fjln45y5.json"
 
 lottie_json = load_lottieurl(lottie_url)
 st_lottie(lottie_json, speed=1, height=200, key="initial")
+
+
+# Grid Setup
+row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns(
+    (0.05, 2, 0.05, 1, 0.05)
+)
+
+# Title
+row0_1.title("한글 노래 가사 n행시")
+
+with row0_2:
+    row0_2.subheader(
+    "Likelion AIS7 Final Project"
+    )
+    st.write('''해파리팀 : 이지혜, 최지영, 권소희, 문종현, 구자현, 김의준''')
+
+st.write('---')
