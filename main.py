f2ed3a2fe6f15324f8625acb375aa0132012cb66
“dk-davidekim <dk.davidekim@gmail.com>
2022-12-25 04:18:57 +0900

    main.py update

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ st_lottie(lottie_json, speed=1, height=200, key="initial")
 
 # Title
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns(
-    (0.01, 2, 0.02, 1, 0.01)
+    (0.01, 2, 0.05, 1, 0.01)
 )
 
 row0_1.title("Korean 노래 가사 n행시")
@@ -36,8 +36,6 @@ with row0_2:
     "Likelion AIS7 Final Project"
     )
     st.write('''해파리팀 : 이지혜, 최지영, 권소희, 문종현, 구자현, 김의준''')
-
-st.write('---')
 
 
 # Explanation
@@ -54,8 +52,10 @@ with row1_1:
         "우리는 음악 가사라 일컬어지는 우리들의 문화를 드러내고 단순히 목적을 위한 사용이 아니라 속한 세대의 특징을 드러낼 수 있는 ‘즐거움’이라는 키워드를 상정하고 이에 걸맞는 프로젝트를 하고자 했다. 그에 따라 sns를 통해 소통하는 MZ세대가 즐겁게 향유할 수 있는 단순하지만 공유를 통해 연결성을 드러낼 수 있는 방식으로 노래 가사 데이터 기반 N행시 생성기를 기획하게 되었다."
     )
 
+st.write('---')
+
 # Model & Input
-row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3 = st.columns((0.01, 1.5, 0.02, 1.5, 0.01))
+row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3 = st.columns((0.01, 1.5, 0.05, 1.5, 0.01))
 
 # def load_model():
 #     return tf.keras.models.load_model('')
